e9d176409096307c71b94b59e00b2e65ccaec70c
Guapura89 <betocrack1228@gmail.com>
2023-02-09 00:53:24 -0700

    :sparkles: feat: Add update button
    - Add update function to localstorage

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 const newName = document.getElementById("in_name");
 const btn_add = document.getElementById("add");
 const list = document.getElementById("list__content");
+const control = document.getElementById("controller");
 
 // Create local storage
 document.addEventListener("DOMContentLoaded", function () {
@@ -44,26 +45,50 @@ document.addEventListener("DOMContentLoaded", function () {
 // Show names in local storage
 function showNames(names, i) {
   const div = document.createElement("div");
+  const btnContainer = document.createElement("div");
   const h4 = document.createElement("h4");
   const content = document.createTextNode(names[i]);
   h4.appendChild(content);
 
-  const btn = document.createElement("button");
-  const btnContent = document.createTextNode("Delete");
-  btn.appendChild(btnContent);
-  btn.onclick = () => {
+  // Create delete button
+  const btnDel = document.createElement("button");
+  const btnDelContent = document.createTextNode("Delete");
+  btnDel.appendChild(btnDelContent);
+  btnDel.classList.add("btn__delete");
+  btnDel.onclick = () => {
     del(i);
   };
 
+  // Create update button
+  const btnUp = document.createElement("button");
+  const btnUpContent = document.createTextNode("Update");
+  btnUp.appendChild(btnUpContent);
+  btnUp.classList.add("btn__update");
+
+  // Add buttons to item container
+  btnContainer.appendChild(btnUp);
+  btnContainer.appendChild(btnDel);
+  btnContainer.classList.add("btn__container");
+
   div.appendChild(h4);
-  div.appendChild(btn);
+  div.appendChild(btnContainer);
 
   list.appendChild(div);
+
+  btnUp.onclick = () => {
+    btnDel.style.backgroundColor = "gray";
+    btnDel.style.color = "initial";
+    btnDel.disabled = true;
+    btnUp.style.backgroundColor = "gray";
+    btnUp.style.color = "initial";
+    btnUp.disabled = true;
+    upd(i);
+  };
 }
 
 // Delete an item in localstorage
 function del(i) {
-  var names = JSON.parse(localStorage.getItem("name_list")) || [];
+  let names = JSON.parse(localStorage.getItem("name_list")) || [];
   names.splice(i, 1);
 
   localStorage.setItem("name_list", JSON.stringify(names));
@@ -76,4 +101,39 @@ function del(i) {
   for (let j = 0; j < names.length; j++) {
     showNames(names, j);
   }
+}
+
+// Update an item in localstorage
+function upd(i) {
+  let names = JSON.parse(localStorage.getItem("name_list")) || [];
+  newName.value = names[i];
+  btn_add.style.display = "none";
+
+  // Create update button in controller section
+  const btna = document.createElement("button");
+  const btnaContent = document.createTextNode("Update");
+  btna.appendChild(btnaContent);
+  btna.classList.add("controller__btnUpdate");
+  control.appendChild(btna);
+
+  // Updating item in localstorage
+  btna.onclick = () => {
+    if (newName.value != "") {
+      names[i] = newName.value;
+
+      localStorage.setItem("name_list", JSON.stringify(names));
+
+      list.innerHTML = "";
+
+      for (let j = 0; j < names.length; j++) {
+        showNames(names, j);
+      }
+
+      newName.value = "";
+      btna.remove();
+      btn_add.style.display = "initial";
+    } else {
+      alert("Ups, You need to enter a name");
+    }
+  };
 }
